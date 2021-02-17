f70cd7dd959bcffced03e56e2a1dee6d6d43fe10
K.Viktor <svry88@gmail.com>
2021-02-17 18:30:08 +0300

    add HW1 ems выставляет ширину поля на указанное количество знаков

--- app/src/main/java/viktor/khlebnikov/geekgrains/android1/calculator/MainActivity.java
+++ app/src/main/java/viktor/khlebnikov/geekgrains/android1/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package viktor.khlebnikov.geekgrains.android1.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button switchButton = findViewById(R.id.switchButton);
+        switchButton.setOnClickListener(view -> {
+            setContentView(R.layout.second_activity);
+
+            Button switchButton2 = findViewById(R.id.switchButton2);
+            switchButton2.setOnClickListener(view2 -> {
+                setContentView(R.layout.activity_main);
+            });
+
+
+        });
+
+
+
+
     }
 }
